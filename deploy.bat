5b36d553af266f0e526767c3a2387cf79a22ded8
Jonathan Bayer <linuxgurugamer@gmail.com>
2019-05-25 20:25:29 -0400

    Added .github/FUNDING.yml

--- deploy.bat
+++ deploy.bat
@@ -7,7 +7,7 @@ rem GAMEDATA is the name of the local GameData
 rem VERSIONFILE is the name of the version file, usually the same as GAMEDATA,
 rem    but not always
 
-set H=R:\KSP_1.5.1_dev
+set H=R:\KSP_1.6.1_dev
 set GAMEDIR=RCSLandAid
 set GAMEDATA="GameData\Diazo"
 set VERSIONFILE=%GAMEDIR%.version
